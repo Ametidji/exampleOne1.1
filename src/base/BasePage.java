d1123fbd5d088c31d684d17b8dae28a541462125
Aminemetidji <amine.metidji@gmail.com>
2019-06-11 13:08:54 -0300

    Therd commit

--- src/base/BasePage.java
+++ src/base/BasePage.java
@@ -1,4 +1,37 @@
 package base;
 
-public class BasePage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class BasePage extends BaseTest {
+
+    protected WebDriver driver;
+    public BasePage(WebDriver idriver){
+        this.driver = idriver;
+    }
+
+    @FindBy(id = "email")
+    public WebElement inputEmail;
+
+    @FindBy(id ="pass")
+    public WebElement inputPassword;
+
+    @FindBy(xpath = "//input[@value='Log In']")
+    public WebElement buttonLogIn;
+
+    public  void buttonLogIn(){
+        buttonLogIn.click();
+
+    }
+
+    public void inputEnterEmail(String userEmail){
+        inputEmail.sendKeys(userEmail);
+    }
+    public void inputEnterPassword(String userPassword){
+        inputPassword.sendKeys(userPassword);
+
+    }
+
+
 }
--- src/tests/FirstTests.java
+++ src/tests/FirstTests.java
@@ -1,67 +1,30 @@
 package tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.FindBy;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+
+import base.BaseTest;
 import org.testng.annotations.Test;
 
-import static base.Settings.*;
 
-public class FirstTests {
 
-    private WebDriver driver = null;
-    private ChromeOptions chromeOptions = null;
+public class FirstTests extends BaseTest {
 
-    @FindBy(id = "email")
-    public WebElement inputEmail;
 
-    @FindBy(id ="pass")
-    public WebElement inputPassword;
-
-    @FindBy(xpath = "//input[@value='Log In']")
-    public WebElement buttonLogIn;
-
-    @BeforeSuite
-    public void setUp(){
-        System.setProperty("webdriver.chrome.driver",RESOURCES + CHROMEDRIVER);
-        chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("Start-maximized");
-        driver = new ChromeDriver(chromeOptions);
-    }
-
-    @AfterSuite
-    public void tearDown(){
-        driver.quit();
-    }
-
-    @BeforeMethod
-    public void  navigateToBaseUrl (){
-        driver.navigate().to(BASE_URL);
-    }
 
     @Test
     public void firstExampleTest() throws InterruptedException {
 
-        driver.findElement(By.id("email")).sendKeys("contact@adress.com");
-        driver.findElement(By.id("pass")).sendKeys("fravega");
-        driver.findElement(By.xpath("//input[@value='Log In']")).click();
-
+        faceBookPage.inputEnterEmail("contact@adress.com");
+        faceBookPage.inputEnterPassword("fravega");
+        faceBookPage.buttonLogIn();
         Thread.sleep(3000);
 
     }
     @Test
     public void secondExampleTest() throws InterruptedException {
 
-        driver.findElement(By.id("email")).sendKeys("contact@adress.com");
-        driver.findElement(By.id("pass")).sendKeys("fravega");
-        driver.findElement(By.xpath("//input[@value='Log In']")).click();
-
+        faceBookPage.inputEnterEmail("contact@adress.com");
+        faceBookPage.inputEnterPassword("fravega");
+        faceBookPage.buttonLogIn();
         Thread.sleep(3000);
 
     }
